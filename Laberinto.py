064e441b1d7dcf45b4ae275d48ba601a587f0b2a
Natalia A. M. M <42217739+NataliaAMM@users.noreply.github.com>
2019-06-05 17:19:39 -0500

    Add files via upload

--- Laberinto.py
+++ Laberinto.py
@@ -1,48 +1,80 @@
-from array import *
-
-laberinto = [x.split() for x in open("laberinto.txt","r").readlines()]
-
-
-#Para el arbol
-class Nodo:
-    def __init__(self, valor,izq=None,der=None):
-        self.valor=valor
-        self.izq=izq
-        self.der=der
+class ArbolNario:
+    def __init__(self, valor = ("", (0, 0)), hijos = []):
+        self.valor = valor
+        self.hijos = hijos
 
 
 
-def encontrar(lista,arbol):
-    if(arbol.valor=='y'):
-        return true
-    #if(arbol==None):
-    
+def abrir():
+    return [x.strip("\n").split(" ") for x in open("laberinto.txt", "r").readlines()]
 
+def EnLista(lista, valor):
+    if lista == []:
+        return False
 
-def rotar(lista):
-    if len(lista)>=2:
-        return list(reversed(map(list, zip(*reversed(lista[::-1])))))
+    return lista[0] == valor or EnLista(lista[1:], valor)
+
+def ListaDeListas(lista, v, f, c):
+    if len(lista) == f:
+        return [0, -1]
+
+    if len(lista[f]) == c:
+        return ListaDeListas(lista, v, f + 1, 0)
+
+    if (lista[f][c] == v):
+        return [f, c]
+
+    return ListaDeListas(lista, v, f, c + 1)
+
+def agregar(valor, lista):
+    lista.append(valor)
     return lista
 
-def encontrarX(lista):
-    if lista==[]:
-        return "No esta x"
-    if recorrer(lista[0]):
-        return "encontrada"
-    return encontrarX(rotar(lista[1:]))
+def arbol(lista, fc, lcoords):
+    if fc[0] == len(lista) or fc[1] == len(lista[0]) or fc[0] < 0 or fc[1] < 0:
+        return ArbolNario((" ", (fc[0], fc[0])), [])
 
+    if EnLista(lcoords, (fc[0], fc[1])):
+        return ArbolNario((lista[fc[0]][fc[1]], (fc[0], fc)[1]), [])
 
-def recorrer(renglon):
-    if len(renglon)==0:
+    if lista[fc[0]][fc[1]] == "1":
+        return ArbolNario(("1", (fc[0], fc[1])), [])
+
+    if lista[fc[0]][fc[1]] == "y":
+        return ArbolNario(("y", (fc[0], fc[1])), [])
+
+    if lista[fc[0]][fc[1]] == "0" or lista[fc[0]][fc[1]] == "x":    
+        return ArbolNario((lista[fc[0]][fc[1]], (fc[0], fc[1])),[
+                    arbol(lista, [fc[0],fc[1]+1], agregar((fc[0], fc[1]), lcoords)),
+                    arbol(lista, [fc[0] + 1, fc[1]], agregar((fc[0], fc[1]), lcoords)),
+                    arbol(lista, [fc[0], fc[1] - 1], agregar((fc[0], fc[1]), lcoords)),
+                    arbol(lista, [fc[0] - 1, fc[1]], agregar((fc[0], fc[1]), lcoords))])
+
+    print ("Solo 1,0,x,y")
+    return ArbolNario(("Error: " + lista[fc[0]][fc[1]], (0, 0)), [])
+
+def buscar(nodo, valor, camino):
+    if nodo == None:
         return False
-    print renglon[0]
-    if(renglon[0]=="x"):
+
+    if nodo.valor[0] == valor:
+        camino.append(nodo.valor[1])
         return True
-    return recorrer(renglon[1:])
+
+    if nodo.hijos == []:
+        return False
+
+    if buscar(nodo.hijos[0], valor, camino) or buscar(nodo.hijos[1], valor, camino)\
+    or buscar(nodo.hijos[2], valor, camino) or buscar(nodo.hijos[3], valor, camino):
+        camino.append(nodo.valor[1])
+        return True
+
+    return False
 
 
-print encontrarX(laberinto)
-#print recorrer(renglon)
-"""
-print laberinto
-"""
+for x in abrir():
+    print x
+print ""
+print("Existe un camino?: " + str(buscar(arbol(abrir(),ListaDeListas(abrir(), "x", 0, 0),[]),"y",[])))
+
+
